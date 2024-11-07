ddb5c8faa68bcf8d8e2438fca43a99ffd79d3143
Cristian Mitu <cristian.mitu@skywindtech.corp>
2024-11-07 11:56:53 +0200

    Setup layout and routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import Home from "./pages/Home";
 import ExplorePage from "./pages/ExplorePage";
 import SearchPage from "./pages/SearchPage";
 import DetailsPage from "./pages/DetailsPage";
+import Layout from "./components/Layout/Layout";
 import "./App.css";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/explore" element={<ExplorePage />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/details" element={<DetailsPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path=":explore" element={<ExplorePage />} />
+          <Route path=":explore/:id" element={<DetailsPage />} />
+          <Route path="/search" element={<SearchPage />} />
+        </Route>
       </Routes>
     </Router>
   );
