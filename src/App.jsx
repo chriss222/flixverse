958f316970fdbc53497a08f91c736b501e525984
Cristian Mitu <cristian.mitu@skywindtech.corp>
2024-11-07 11:44:14 +0200

    setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ExplorePage from "./pages/ExplorePage";
+import SearchPage from "./pages/SearchPage";
+import DetailsPage from "./pages/DetailsPage";
 import "./App.css";
 
-function App() {
-  return <main className="text-lg text-red-500 font-bold">hello world</main>;
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<ExplorePage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/details" element={<DetailsPage />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App;
